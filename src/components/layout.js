f0b094dbebe79f3d0843df186fab5249560faa61
apareja <alexis.pareja@hotmail.com>
2021-12-22 12:56:51 -0500

    adds header style

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,14 @@
 
 import * as React from "react"
 import Header from "./header" 
+import { GlobalStyle } from "./styles/GlobalStyles"
 
 const Layout = ({ children }) => {
 
 
   return (
     <>
+    <GlobalStyle />
      <Header/>
         <main>{children}</main>       
     </>
